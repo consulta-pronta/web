f6c38e4ac106eb1cb20f501f95f3caac1a9dd218
Fernando <fernandofkz@hotmail.com>
2026-08-08 19:42:14 -0300

    fix route name, add RouterView in app.vue
    Added RouterView for full functionality
    
    work on: #3

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,12 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-            path: '/cadastroWebPaciente',
+            path: '/CadastroWebPaciente',
             name: 'cadastroPaciente',
             component: () => import('../CadastroWebPaciente.vue')
         },
         {
-            path: '/cadastroWebProfissional',
+            path: '/CadastroWebProfissional',
             name: 'cadastroProfissional',
             component: () => import('../CadastroWebProfissional.vue')
         }
