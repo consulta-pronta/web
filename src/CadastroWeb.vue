2a0ab59b93be6bfa272f637fac77c89347322a65
Fernando <fernandofkz@hotmail.com>
2026-08-08 19:05:30 -0300

    fix cadastroWeb name, add ButtonAccent.vue
    Rename for future professional inclusion.
    
    works on: #3

--- src/CadastroWeb.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-	import img_background from '@/components/img_background.vue';
-</script>
-
-<template>
-	<img_background />
-</template>
-
-<style scoped></style>
---
+++ src/components/ButtonAccent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+})
+</script>
+<template>
+    <button type="button" class="bg-accent px-5 rounded-md w-125.75 h-11 flex items-center justify-center text-textDark cursor-pointer">
+        {{ text }}
+    </button>
+</template>
