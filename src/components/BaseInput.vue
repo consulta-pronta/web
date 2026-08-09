cdd5a0732a2537048724c70e25a7f59d821bd332
vtkaxx <vtkaxx@proton.me>
2026-08-09 11:42:13 -0300

    add props to BaseInput.vue and class to logo
    Props:
    
    - type: string
    - icon: string
    
    Class:
    
    - select-none
    
    Work on: #6

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 interface Props {
+    type: string
     placeholder: string
+    icon: string
 }
 
 const props = defineProps<Props>()
 </script>
 
 <template>
-    <input type="text" :placeholder="props.placeholder" class="bg-primaryBlue px-5 rounded-md w-125.75 h-11.75 flex items-center text-textlight placeholder:text-textlight outline-none">
+    <div class="relative flex items-center">
+        <span class="material-symbols-rounded text-textlight absolute left-3 text-xl pointer-events-none">{{ icon }}</span>
+        <input :type="props.type" :placeholder="props.placeholder" class="bg-primaryBlue px-5 rounded-md w-125.75 h-11.75 flex items-center text-textlight placeholder:text-textlight outline-none pl-10">
+    </div>
 </template>
--- src/components/CompleteLogo.vue
+++ src/components/CompleteLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-1/2 box-border p-16">
+    <div class="w-1/2 box-border p-16 select-none">
             <div class="mb-4">
                 <img src="/img/LogoTemporaria.png">
             </div>
