318ad6c602d3875329f0f9f6b038016c7c582c6b
Fernando <fernandofkz@hotmail.com>
2026-08-15 17:51:29 -0300

    feat location component
    turned location tag into a component

---
+++ src/components/LocationTag.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+	localizacao?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	localizacao: 'Costas',
+})
+</script>
+
+<template>
+    <div class="flex items-center text-primarydark bg-accent rounded-md w-fit p-1">
+        <span class="material-symbols-rounded text-[12px]! mr-1">
+            location_on
+        </span>
+
+        <p class="text-[12px]">
+            {{ props.localizacao }}
+        </p>
+    </div>
+</template>
