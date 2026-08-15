8a9d8964a69ff21e2bed107f97871cfbfc134f05
Fernando <fernandofkz@hotmail.com>
2026-08-15 18:30:16 -0300

    feat descblock

--- src/pages/HistoricoUsuario.vue
+++ src/pages/HistoricoUsuario.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 import BaseButton from '@/components/BaseButton.vue';
 import FilterOrd from '@/components/FilterOrd.vue';
+import IntensityComp from '@/components/IntensityComp.vue';
+import LocationTag from '@/components/LocationTag.vue';
 import NavBar from '@/components/NavBar.vue';
 import SymptomBlock from '@/components/SymptomBlock.vue';
+import UserPhoto from '@/components/UserPhoto.vue';
+import { ref } from 'vue';
+
+const Show = ref(false)
+
+function toggle() {
+	Show.value = !Show.value
+}
 </script>
 
 <template class="justify-end">
@@ -29,11 +39,12 @@ import SymptomBlock from '@/components/SymptomBlock.vue';
 
 				<!--Sliders-->
 				<FilterOrd class="mb-3"/>
-				<!--Blocos de historico-->
+
+				<!--Blocos de historicos-->
 				<p class="text-textlight text-[24px]">4 sintomas registrados</p>
 
 				<section class="flex h-[82.5%]">
-					<div class="w-[40%] h-full flex flex-col gap-y-2 overflow-y-auto scrollbar-hide ">
+					<div class="w-[40%] h-full flex flex-col gap-y-2 overflow-y-auto scrollbar-hide">
 						<SymptomBlock
 						:sintoma-persistente="true"
 						Cor="warning"
@@ -55,9 +66,61 @@ import SymptomBlock from '@/components/SymptomBlock.vue';
 						localizacao="Mindinho"
 						/>
 					</div>
+					<!--Especificado-->
+					<div class="w-[60%] h-full flex flex-col m-2 overflow-y-auto scrollbar-hide">
+						<section class="flex justify-center place-items-center w-full h-[9%] relative mb-3">
+							<span class="material-symbols-rounded absolute left-0 text-[38px]! text-textlight">
+								arrow_back
+							</span>
+							<div class="flex justify-center w-[88%] h-full bg-surface place-items-center rounded-md">
+								<span class="material-symbols-rounded text-primarydark pointer-events-none mr-1">
+									edit
+								</span>
+								<p class="text-[18px] text-primarydark text-semibold">Editar sintomas</p>
+							</div>
+						</section>
 
-					<div class="w-[60%]">
-						a
+						<section>
+							<div class="w-full h-fit bg-surface rounded-[15px] p-2">
+
+								<section class="flex justify-between">
+									<article class="text-primarydark flex justify-center">
+										<span class="material-symbols-rounded text-[28px]! mr-1">
+											calendar_month
+										</span>
+										<p class="text-[16px]">
+											dia/mes/ano
+										</p>
+									</article>
+
+									<button type="button" @click="toggle">
+										<span class="material-symbols-rounded text-[32px]! mr-1 text-primarydark">
+											{{ Show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}
+										</span>
+									</button>
+								</section>
+
+								<section  v-if="Show === true" class="border border-primarydark rounded-lg p-2">
+									<IntensityComp Cor="warning" intensidade="6" class="mb-2"/>
+
+									<p class="text-primatydark font-bold text-[16px] mb-2">
+										Dor nas
+									</p>
+
+									<p class="text-primatydark font-light text-[12px] mb-2">
+										Descrição
+									</p>
+
+									<LocationTag localizacao="Perna" bgcolor="primarydark" textcolor="surface" class="mb-2"/>
+
+									<section class="border-t border-primarydark p-2">
+										<UserPhoto rounded="0" :circle="false" borderColor="primarydark"/>
+									</section>
+
+								</section>
+							</div>
+						</section>
+
 					</div>
 				</section>
 
