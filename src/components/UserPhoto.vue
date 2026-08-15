db1277997141298da2df707795f6837dbef62c3e
Fernando <fernandofkz@hotmail.com>
2026-08-15 18:29:49 -0300

    fix UserPhoto
    turned some class in variables

--- src/components/UserPhoto.vue
+++ src/components/UserPhoto.vue
@@ -4,6 +4,23 @@ import { ref, onBeforeUnmount } from "vue"
 const selectedFile = ref<File | null>(null)
 const previewUrl = ref<string | null>(null)
 
+
+interface Props {
+	w?: string
+	h?: string
+	rounded?: string | number
+	circle?: boolean
+	borderColor?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	w: "19.75",
+	h: "19.75",
+	rounded: "full",
+	circle: true,
+	borderColor: "accent",
+})
+
 function handleFileSelect(event: Event) {
 	const input = event.target as HTMLInputElement
 	const file = input.files?.[0]
@@ -27,9 +44,7 @@ onBeforeUnmount(() => {
 
 <template>
 	<article class="flex flex-row relative inline-block">
-		<label
-			class="w-19.75 h-19.75 bg-primaryBlue rounded-full border-2 border-accent cursor-pointer overflow-hidden flex items-center justify-center"
-		>
+		<label :class="[`w-${props.w} h-${props.h} bg-primaryBlue rounded-${props.rounded} border-2 border-${props.borderColor} cursor-pointer overflow-hidden flex items-center justify-center`]">
 			<input type="file" accept=".jpg,.jpeg,.png" class="hidden" @change="handleFileSelect" />
 
 			<img
@@ -44,9 +59,7 @@ onBeforeUnmount(() => {
 			</span>
 		</label>
 
-		<div
-			class="absolute-right w-5.75 h-5.75 rounded-full bg-accent absolute bottom-0 right-0 flex items-center justify-center"
-		>
+		<div v-if="props.circle" class="absolute-right w-5.75 h-5.75 rounded-full bg-accent absolute bottom-0 right-0 flex items-center justify-center">
 			<section class="material-symbols-rounded text-xl/10!">photo_camera</section>
 		</div>
 	</article>
