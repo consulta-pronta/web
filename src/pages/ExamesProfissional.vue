0a8ff73c81002fca8cfa4d22ae8b2f4a4f2d166d
Fernando <fernandofkz@hotmail.com>
2026-08-23 21:15:33 -0300

    add request exam overlay
    closes on #22

--- src/pages/ExamesProfissional.vue
+++ src/pages/ExamesProfissional.vue
@@ -6,6 +6,8 @@ import BaseButton from '@/components/BaseButton.vue';
 import FilterOrd from '@/components/FilterOrd.vue';
 
 const solicitacao = ref(false)
+const dropdownExameAberto = ref(false)
+const exameSelecionado = ref('')
 
 interface Exame {
 	nome: string
@@ -42,6 +44,14 @@ function toggleSolicitacao() {
 	solicitacao.value = !solicitacao.value
 }
 
+function dropExam() {
+	dropdownExameAberto.value = !dropdownExameAberto.value
+}
+
+function selecionarExame() {
+	exameSelecionado.value = 'Heredograma'
+	dropdownExameAberto.value = false
+}
 
 </script>
 
@@ -74,7 +84,7 @@ function toggleSolicitacao() {
 
 
 						<div v-if="solicitacao === true" class="absolute inset-0 z-50 bg-black/50 w-full h-full flex justify-center place-items-center">
-							<form class="bg-background w-[52%] h-[95%] rounded-3xl px-15 py-5 flex flex-col place-items-center relative">
+							<form class="bg-background w-[52%] rounded-3xl px-15 py-5 flex flex-col place-items-center relative">
 								<button type="button" @click="toggleSolicitacao">
 									<span class="material-symbols-rounded absolute left-0 text-4xl! text-textLight ml-14 cursor-pointer">
 										arrow_back
@@ -86,37 +96,94 @@ function toggleSolicitacao() {
 								</p>
 
 								<div class="relative w-full mt-3">
-									<span class="material-symbols-rounded text-primaryDark absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
-										person
-									</span>
-
-									<select name="intensidade" required class="w-full h-11 rounded-md bg-textLight text-primaryDark px-4 pl-10 appearance-none outline-none">
-										<option value="" disabled selected>
-											Selecione o paciente
-										</option>
-										<option value="1">Cláudio Silva</option>
-										<option value="2">The Robert</option>
-									</select>
-								</div>
-
-								<span class="mt-3 w-full">
-									<FilterOrd :order="false" status="typing" class="absolute left"/>
-								</span>
-
-								<p class="text-textLight text-base text-semibold mt-2 w-full">
-									Pesquisar e Selecionar exames
-								</p>
-
-								<section class="relative mt-3 w-full flex justify-center">
-									<div class="relative flex items-center w-full h-11 bg-textLight rounded-md">
-										<span class="material-symbols-rounded text-primaryDark absolute left-3 pointer-events-none">
-											search
+										<span class="material-symbols-rounded text-primaryDark absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none">
+											person
 										</span>
 
-										<input type="text" placeholder="Ex. Hemograma" class="text-primaryDark placeholder-primaryDark w-full h-full outline-none pl-10"/>
+										<select name="paciente" required class="w-full h-11 rounded-md bg-textLight text-primaryDark px-4 pl-10 appearance-none outline-none">
+											<option value="" disabled selected>
+												Selecione o paciente
+											</option>
+											<option value="1">Cláudio Silva</option>
+											<option value="2">The Robert</option>
+										</select>
 									</div>
-								</section>
 
+									<span class="mt-3 w-full">
+										<FilterOrd :order="false" status="typing" class="absolute left"/>
+									</span>
+
+									<p class="text-textLight text-base text-semibold mt-2 w-full">
+										Pesquisar e Selecionar exames
+									</p>
+
+									<section class="relative mt-3 w-full flex justify-center">
+										<div class="relative flex items-center w-full h-11 bg-textLight rounded-md">
+											<span class="material-symbols-rounded text-primaryDark absolute left-3 pointer-events-none">
+												search
+											</span>
+
+											<input type="text" placeholder="Ex. Hemograma" class="text-primaryDark placeholder-primaryDark w-full h-full outline-none pl-10"/>
+										</div>
+									</section>
+
+									<div class="relative w-full h-full mt-3">
+
+										<button type="button" @click="dropExam" class="w-full h-11 bg-background text-textLight outline-none flex items-center">
+											<span class="material-symbols-rounded">
+												{{ dropdownExameAberto ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}
+											</span>
+											<span class="font-bold text-lg">
+												Selecione o exame
+											</span>
+										</button>
+
+										<div v-if="dropdownExameAberto" class="flex flex-col z-50 mt-1 w-full h-67 rounded-md overflow-y-scroll scrollbar-hide"> <!--problema futuro quanto a responsividade-->
+											<button type="button" @click="selecionarExame" class="w-full px-4 py-3 flex items-center gap-3 text-left text-primaryDark cursor-pointer bg-textLight">
+												<span class="material-symbols-rounded">
+													bloodtype
+												</span>
+												<div>
+													<p class="font-semibold">Heredograma</p>
+													<p class="text-sm">Laboratorial</p>
+												</div>
+											</button>
+
+											<button type="button" @click="selecionarExame" class="w-full px-4 py-3 flex items-center gap-3 text-left text-primaryDark cursor-pointer bg-textLight">
+												<span class="material-symbols-rounded">
+													bloodtype
+												</span>
+												<div>
+													<p class="font-semibold">Glicemia em jejum</p>
+													<p class="text-sm">Laboratorial</p>
+												</div>
+											</button>
+
+											<button type="button" @click="selecionarExame" class="w-full px-4 py-3 flex items-center gap-3 text-left text-primaryDark cursor-pointer bg-textLight">
+												<span class="material-symbols-rounded">
+													monitor_heart
+												</span>
+												<div>
+													<p class="font-semibold">Eletrocardiograma</p>
+													<p class="text-sm">Gráfico</p>
+												</div>
+											</button>
+
+											<button type="button" @click="selecionarExame" class="w-full px-4 py-3 flex items-center gap-3 text-left text-primaryDark cursor-pointer bg-textLight">
+												<span class="material-symbols-rounded">
+													radiology
+												</span>
+												<div>
+													<p class="font-semibold">Raio-x</p>
+													<p class="text-sm">Imagem</p>
+												</div>
+											</button>
+
+									</div>
+
+									<BaseButton type="submit" text="Confirmar e Solicitar Exame" class="w-full mt-2"/>
+
+								</div>
 
 							</form>
 						</div>
