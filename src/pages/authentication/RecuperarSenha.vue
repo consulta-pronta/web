b7a83de745364ea21f239f7dafc450bf7fc7a7ca
leafcabral <rafaelcabral4100@gmail.com>
2026-09-06 10:33:03 -0300

    add integration with firebase auth for reset password

--- src/pages/authentication/RecuperarSenha.vue
+++ src/pages/authentication/RecuperarSenha.vue
@@ -1,161 +1,98 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, watch } from "vue"
 import { useRouter } from "vue-router"
-
 import AuthBackground from "@/components/AuthBackground.vue"
-import BaseButton from "@/components/bases/BaseButton.vue"
+import BaseButton, { type ButtonState } from "@/components/bases/BaseButton.vue"
 import BaseInput from "@/components/bases/BaseInput.vue"
 import BaseLogo from "@/components/bases/BaseLogo.vue"
+import { auth } from "@/config/firebase"
+import { sendPasswordResetEmail } from "firebase/auth"
+import validator from "validator"
 
 const router = useRouter()
 
-const passo = ref(1)
-const prosseguir = () => {
-	if (passo.value === 3) {
-		router.push("login")
+const email = ref("")
+const submitState = ref<ButtonState>("disabled")
+const emailSent = ref(false)
+
+const submitForm = async () => {
+	if (emailSent.value) {
+		router.back()
+		return
 	}
-	passo.value++
+
+	submitState.value = "sync"
+
+	await sendPasswordResetEmail(auth, email.value)
+
+	emailSent.value = true
+	stopWatcing()
+	submitState.value = "enabled"
 }
 
-const voltar = () => {
-	if (passo.value === 1) {
-		router.push("login")
-	}
-	passo.value--
-}
-
-// Excluir depois, só pra n ficar com codigo vermelho
-const bruh = ref('')
+const stopWatcing = watch(email, (value) => {
+	submitState.value = validator.isEmail(value) ? "enabled" : "disabled"
+})
 </script>
 
 <template>
 	<AuthBackground>
-		<BaseLogo text complete class="p-8!" />
-		<main class="lg:w-1/2 box-border items-center justify-center flex flex-col lg:relative">
+		<BaseLogo text complete />
+
+		<main
+			class="lg:w-1/2 box-border items-center justify-center flex flex-col gap-6 lg:relative"
+		>
 			<BaseButton
 				theme="textLight"
 				mode="transparent"
 				icon="arrow_back"
 				class="lg:absolute lg:top-0 lg:left-0 mb-3 sm:m-5"
-				@click="voltar()"
+				@click="router.back()"
 			>
 				<p class="text-3xl font-bold">Voltar</p>
 			</BaseButton>
-			<section class="box-border items-center justify-center flex flex-col">
-				<div class="text-4xl sm:text-5xl xl:text-6xl text-surface font-bold mb-1">
-					Recuperar senha
-				</div>
-				<div class="text-lg sm:text-xl xl:text-2xl text-surface mb-5 text-center">
-					Siga os passos abaixo para recuperar sua senha.
-				</div>
 
-				<hr class="h-px border-0 bg-textLight w-90 sm:w-120 xl:w-140 my-1 sm:my-3 mx-auto rounded-full" />
+			<header class="text-textLight text-center flex flex-col gap-1">
+				<div class="text-4xl sm:text-5xl xl:text-6xl font-bold">Recuperar senha</div>
+				<div class="text-lg sm:text-xl xl:text-2xl">
+					Siga os passos abaixo para recuperar sua senha
+				</div>
+			</header>
 
-				<form @submit.prevent="prosseguir()"
-					class="space-y-2 items-center justify-center flex flex-col p-4 w-100 sm:w-120 lg:w-120 xl:w-140"
-				>
-					<section class="flex flex-col w-full justify-start gap-3 mb-6 text-accent">
-						<p :class="passo !== 1 ? 'opacity-70' : ''">
-							1. Digite seu e-mail
-						</p>
-						<p v-if="passo >= 2" :class="passo !== 2 ? 'opacity-70' : ''">
-							2. Digite o código enviado por e-mail
-						</p>
-						<p v-if="passo == 3">
-							3. Digite sua nova senha
-						</p>
-					</section>
+			<form
+				@submit.prevent="submitForm"
+				class="space-y-2 items-center justify-center flex flex-col p-4 w-100 sm:w-120 lg:w-120 xl:w-140"
+			>
+				<!-- Depois fazer isso se tornar barte do BaseInput -->
+				<label class="w-full flex flex-col justify-start gap-2" v-if="!emailSent">
+					<p class="text-accent/85">Digite o seu e-mail</p>
 
 					<BaseInput
-						v-if="passo === 1"
 						type="email"
-						theme="dark"
 						placeholder="Email"
 						icon="email"
-						class="w-full"
-						required
-					/>
-
-					<section v-if="passo === 2" class="flex justify-center items-center gap-3">
-						<input
-							v-for="n in 6"
-							:key="n"
-							type="text"
-							required
-							class="w-full h-30 text-4xl text-textLight bg-primaryDark outline-2 outline-primary rounded-2xl p-3 text-center"
-						>
-					</section>
-
-					<!-- Trocar para uma store de recuperar senha (Rafael) -->
-					<div
-						v-if="passo === 3"
-						class="relative w-full"
-					>
-						<BaseInput
-							type="password"
-							placeholder="Senha"
-							icon="lock"
-							theme="dark"
-							v-model="bruh"
-							required
-							@focusin="bruh"
-							@focusout="bruh"
-						/>
-						<div
-							v-if="bruh"
-							class="absolute left-0 right-0 bottom-[130%] mx-auto w-64 bg-surface p-3"
-						>
-							<p
-								v-for="(value, key) in bruh"
-								:key="key"
-								:class="value ? 'text-textDark' : 'text-error'"
-								class="flex items-center gap-2 text-sm py-0.5"
-							>
-								<span class="material-symbols-rounded">
-									{{ value ? "check_circle" : "cancel" }}
-								</span>
-								{{
-									key === "minLength"
-										? "Mínimo 8 caracteres"
-										: key === "hasNumber"
-											? "Pelo menos 1 número"
-											: key === "hasLowercase"
-												? "Pelo menos 1 letra minúscula"
-												: key === "hasUppercase"
-													? "Pelo menos 1 letra maiúscula"
-													: key === "match"
-														? "Senhas coincidem"
-														: key
-								}}
-							</p>
-							<span
-								class="w-4 h-4 bg-surface absolute -bottom-2 left-0 right-0 mx-auto rotate-45"
-							></span>
-						</div>
-					</div>
-
-					<BaseInput
-						v-if="passo === 3"
-						type="password"
-						placeholder="Confirmar senha"
-						icon="lock"
 						theme="dark"
-						class="w-full"
-						v-model="bruh"
 						required
+						v-model="email"
 					/>
+				</label>
+				<div v-else class="text-accent/85 text-center">
+					<p>Abra o e-mail enviado para prosseguir com a recuperação de senha.</p>
+					<p>Após isso, volte para a página de login para entrar em sua conta.</p>
+				</div>
 
-					<br>
+				<br />
 
-					<BaseButton
-						type="submit"
-						theme="accent"
-						class="justify-center w-full mt-4"
-					>
-						Prosseguir
-					</BaseButton>
-				</form>
-			</section>
+				<BaseButton
+					type="submit"
+					theme="accent"
+					class="justify-center w-full mt-4"
+					:state="submitState"
+				>
+					<template v-if="emailSent"> Voltar para página de login </template>
+					<template v-else> Enviar e-mail para resetar senha </template>
+				</BaseButton>
+			</form>
 		</main>
 	</AuthBackground>
 </template>
