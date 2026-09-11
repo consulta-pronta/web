01de731a1a151b4bafbc3962501a4241de11ab3b
vtkaxx <vtkaxx@proton.me>
2026-09-11 10:34:14 -0300

    fix view report dialog and change report actions td to dropdown

--- src/components/ReportRow.vue
+++ src/components/ReportRow.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { ref, useTemplateRef } from 'vue'
+import { useTemplateRef } from 'vue'
 import BaseDialog from '@/components/bases/BaseDialog.vue'
+import BaseButton from '@/components/bases/BaseButton.vue'
 
 interface Report {
 	id: string
 	nome: string
 	data: string
 	horario: string
-	Show: boolean
+	show: boolean
 }
 
 const props = defineProps<{
@@ -15,7 +16,7 @@ const props = defineProps<{
 }>()
 
 function toggle(report: Report) {
-	report.Show = !report.Show
+	report.show = !report.show
 }
 
 const allowedProfessionals = useTemplateRef('allowedProfessionals')
@@ -36,96 +37,53 @@ const viewReport = useTemplateRef('viewReport')
 			</p>
 		</td>
 
-		<td>{{ report.id }}</td>
+		<td class="text-center">{{ report.id }}</td>
 
-		<td>{{ report.data }}</td>
+		<td class="text-center">{{ report.data }}</td>
 
-		<td>
-			<div class="flex justify-center items-center relative">
+		<td class="relative">
+			<div class="flex justify-center items-center">
 				{{ report.horario }}
-				<button type="button" @click="toggle(report)" class="absolute right-1 md:right-2 cursor-pointer">
+				<button type="button" @click="toggle(report)" class="absolute right-1 md:right-2 cursor-pointer anchor-name-[--botoeira]">
 					<span class="material-symbols-rounded text-primaryDark">
-						{{ report.Show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}
+						{{ 'more_horiz' }}
 					</span>
 				</button>
 			</div>
 		</td>
+
+		<div v-if="report.show" class="flex flex-col text-primaryDark absolute right-0 top-[anchor(top)] bg-surface z-10 outline-1 outline-primary rounded-lg p-1 position-anchor-[--botoeira]">
+			<BaseButton icon="download" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Baixar relatório
+			</BaseButton>
+			<BaseButton @click="allowedProfessionals" icon="shield_toggle" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Profissionais permitidos
+			</BaseButton>
+			<BaseButton @click="formRename" icon="edit_square" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Renomear relatório
+			</BaseButton>
+			<BaseButton icon="delete" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Apagar relatório
+			</BaseButton>
+			<BaseButton @click="definePassword" icon="lock" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Definir senha
+			</BaseButton>
+			<BaseButton @click="viewReport?.toggle()" icon="visibility" theme="primaryDark" mode="transparent" class="text-sm! p-2! justify-start">
+				Visualizar relatório
+			</BaseButton>
+		</div>
 	</tr>
 
-	<tr v-if="report.Show === true" class="col-span-4 text-sm">
-		<td colspan="4">
-			<div class="flex justify-between px-16 lg:px-8 py-4">
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						download
-					</span>
-					<p class="hidden lg:block text-xs">
-						Baixar Relatório
-					</p>
-				</button>
-
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer" @click="allowedProfessionals">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						shield_toggle
-					</span>
-					<p class="hidden lg:block text-xs">
-						Profissionais Permitidos
-					</p>
-				</button>
-
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer" @click="formRename">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						edit_square
-					</span>
-					<p class="hidden lg:block text-2xl! lg:text-xs!">
-						Renomear Relatório
-					</p>
-				</button>
-
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						delete
-					</span>
-					<p class="hidden lg:block text-xs">
-						Apagar Relatório
-					</p>
-				</button>
-
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer" @click="definePassword">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						lock
-					</span>
-					<p class="hidden lg:block text-xs!">
-						Definir Senha
-					</p>
-				</button>
-
-				<button type="button" class="flex flex-col text-primaryDark cursor-pointer" @click="viewReport?.toggle()">
-					<span class="material-symbols-rounded text-2xl! lg:text-sm!">
-						visibility
-					</span>
-					<p class="hidden lg:block text-xs!">
-						Visualizar Relatório
-					</p>
-				</button>
-			</div>
-		</td>
-	</tr>
-
-
-
-	<BaseDialog ref="viewReport">
-		<form class="flex flex-col">
-			<article>
-
-				<!--Textos-->
-				<p class="text-textLight text-2xl font-bold">
-					Dor Constante
-				</p>
-				<p class="text-textLight text-base font-light italic">
+	<BaseDialog title="Dor constante" ref="viewReport">
+		<form class="flex flex-col gap-2 overflow-y-auto max-h-[80vh]">
+			<!--Textos-->
+			<section class="text-textLight">
+				<p class="text-base font-light italic">
 					Relatório: ID51966
 				</p>
-				<span class="flex text-textLight text-base">
+			</section>
+			<section class="text-textLight">
+				<span class="flex text-base">
 					<p class="font-bold">
 						Período:
 					</p>
@@ -133,7 +91,7 @@ const viewReport = useTemplateRef('viewReport')
 						10 de abr 2026 a 18 abr 2026
 					</p>
 				</span>
-				<span class="flex text-textLight text-base">
+				<span class="flex text-base">
 					<p class="font-bold">
 						Duração:
 					</p>
@@ -141,29 +99,31 @@ const viewReport = useTemplateRef('viewReport')
 						8 dias (20 dias)
 					</p>
 				</span>
+			</section>
 
-				<!--Paciente-->
-				<section class="flex just mx-3 mt-3">
-					<UserPhoto/>
-					<article class="flex flex-col justify-center mx-4">
-						<p class="text-textLight text-base font-bold">
-							Cláudio Silva
-						</p>
-						<p class="text-textLight text-xs">
-							claudiosilva213@email.com
-						</p>
-						<p class="text-textLight text-xs">
-							(27) 99722-3725
-						</p>
-					</article>
-				</section>
+			<!--Paciente-->
+			<section class="flex just">
+				<UserPhoto/>
+				<article class="flex flex-col text-textLight text-xs justify-center">
+					<p class="text-base font-bold">
+						Cláudio Silva
+					</p>
+					<p>
+						claudiosilva213@email.com
+					</p>
+					<p>
+						(27) 99722-3725
+					</p>
+				</article>
+			</section>
 
-				<!--Informações-->
-				<p class="text-textLight text-xl font-bold mx-4 mt-2">
+			<!--Informações-->
+			<article>
+				<p class="text-textLight text-xl font-bold mt-2">
 					Resumo Geral
 				</p>
-				<section class="grid grid-cols-2 gap-2 mx-4 mt-1 w-[95%] h-[18%] text-background">
-					<div class="bg-surface rounded-xl flex flex-col p-2.5">
+				<section class="flex w-full p-2 gap-2 text-background">
+					<div class="bg-surface rounded-xl flex flex-col w-full p-2.5">
 						<p class="text-sm font-medium">
 							Sintoma mais comum:
 						</p>
@@ -173,7 +133,7 @@ const viewReport = useTemplateRef('viewReport')
 						<p class="text-sm font-medium">
 							2 semanas
 						</p>
-						<div class="flex bg-error rounded-3xl text-surface mt-1 w-[55%] md:w-[60%] lg:w-[45%] h-[30%] md:h-[35%] lg:h-[45%] items-center">
+						<div class="flex bg-error rounded-3xl text-surface w-fit items-center p-1">
 							<span class="material-symbols-rounded text-xs! mx-1.5">
 								warning
 							</span>
@@ -183,9 +143,9 @@ const viewReport = useTemplateRef('viewReport')
 						</div>
 					</div>
 
-					<div class="flex flex-col bg-surface text-background rounded-xl p-2.5">
+					<div class="flex flex-col w-full bg-surface text-background rounded-xl p-2.5">
 						<span class="flex">
-							<p class="text-base font-bold mx-4">
+							<p class="text-base font-bold">
 								10/04:
 							</p>
 							<p class="text-error text-base font-normal mx-0.5">
@@ -193,7 +153,7 @@ const viewReport = useTemplateRef('viewReport')
 							</p>
 						</span>
 						<span class="flex">
-							<p class="text-base font-bold mx-4">
+							<p class="text-base font-bold">
 								12/04:
 							</p>
 							<p class="text-sucess text-base font-normal mx-0.5">
@@ -201,7 +161,7 @@ const viewReport = useTemplateRef('viewReport')
 							</p>
 						</span>
 						<span class="flex">
-							<p class="text-base font-bold mx-4">
+							<p class="text-base font-bold">
 								14/04:
 							</p>
 							<p class="text-warning text-base font-normal mx-0.5">
@@ -210,110 +170,109 @@ const viewReport = useTemplateRef('viewReport')
 						</span>
 					</div>
 				</section>
+			</article>
 
-				<!--Cronologia-->
-				<p class="text-textLight text-xl font-bold mx-4 mt-10 md:mt-12 lg:mt-8">
-					Cronologia do Sintoma
+			<!--Cronologia-->
+			<p class="text-textLight text-xl font-bold">
+				Cronologia do Sintoma
+			</p>
+
+			<section class="flex flex-col gap-1 p-2 max-h-full">
+				<div class="flex">
+					<article class="flex flex-col justify-center items-center w-10">
+						<div class="flex justify-center items-center">
+							<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
+								vital_signs
+							</span>
+						</div>
+						<hr class="flex-1 w-px bg-primaryLight border-0">
+					</article>
+					<article class="flex flex-col w-full bg-surface text-background rounded-lg p-3">
+						<p class="text-lg font-bold">
+							Dor nas costas ao levantar peso
+						</p>
+						<p class="text-sm font-medium">
+							(16 abr. 2026)
+						</p>
+						<p class="text-sm font-medium">
+							- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
+						</p>
+					</article>
+				</div>
+
+				<div class="flex">
+					<article class="flex flex-col justify-center items-center w-10">
+						<div class="flex justify-center items-center">
+							<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
+								vital_signs
+							</span>
+						</div>
+						<hr class="flex-1 w-px bg-primaryLight border-0">
+					</article>
+					<article class="flex flex-col w-full bg-surface text-background rounded-lg p-3">
+						<p class="text-lg font-bold">
+							Dor nas costas ao levantar peso
+						</p>
+						<p class="text-sm font-medium">
+							(16 abr. 2026)
+						</p>
+						<p class="text-sm font-medium">
+							- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
+						</p>
+					</article>
+				</div>
+
+				<div class="flex">
+					<article class="flex flex-col justify-center items-center w-10">
+						<div class="flex justify-center items-center">
+							<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
+								vital_signs
+							</span>
+						</div>
+						<hr class="flex-1 w-px bg-primaryLight border-0">
+					</article>
+					<article class="flex flex-col w-full bg-surface text-background rounded-lg p-3">
+						<p class="text-lg font-bold">
+							Dor nas costas ao levantar peso
+						</p>
+						<p class="text-sm font-medium">
+							(16 abr. 2026)
+						</p>
+						<p class="text-sm font-medium">
+							- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
+						</p>
+					</article>
+				</div>
+
+				<!--Visão geral-->
+				<p class="text-textLight text-xl font-bold mt-2">
+					Visão Geral
 				</p>
 
-				<section class="flex flex-col p-4 max-h-full">
-
-					<div class="flex">
-						<article class="flex flex-col justify-center items-center w-[10%]">
-							<div class="flex justify-center items-center">
-								<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
-									vital_signs
-								</span>
-							</div>
-							<hr class="flex-1 w-px bg-primaryLight border-0">
-						</article>
-						<article class="flex flex-col w-[90%] h-[90%] bg-surface text-background rounded-lg p-3">
-							<p class="text-lg font-bold">
-								Dor nas costas ao levantar peso
-							</p>
-							<p class="text-sm font-medium">
-								(16 abr. 2026)
-							</p>
-							<p class="text-sm font-medium">
-								- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
-							</p>
-						</article>
-					</div>
-
-					<div class="flex">
-						<article class="flex flex-col justify-center items-center w-[10%]">
-							<div class="flex justify-center items-center">
-								<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
-									vital_signs
-								</span>
-							</div>
-							<hr class="flex-1 w-px bg-primaryLight border-0">
-						</article>
-						<article class="flex flex-col w-[90%] h-[90%] bg-surface text-background rounded-lg p-3">
-							<p class="text-lg font-bold">
-								Dor nas costas ao levantar peso
-							</p>
-							<p class="text-sm font-medium">
-								(16 abr. 2026)
-							</p>
-							<p class="text-sm font-medium">
-								- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
-							</p>
-						</article>
-					</div>
-
-					<div class="flex">
-						<article class="flex flex-col justify-center items-center w-[10%]">
-							<div class="flex justify-center items-center">
-								<span class="material-symbols-rounded bg-primaryLight text-background rounded-full">
-									vital_signs
-								</span>
-							</div>
-							<hr class="flex-1 w-px bg-primaryLight border-0">
-						</article>
-						<article class="flex flex-col w-[90%] h-[90%] bg-surface text-background rounded-lg p-3">
-							<p class="text-lg font-bold">
-								Dor nas costas ao levantar peso
-							</p>
-							<p class="text-sm font-medium">
-								(16 abr. 2026)
-							</p>
-							<p class="text-sm font-medium">
-								- Intensidade 6/10; "Começa a doer um pouco despois de eu levantar, mas para de doer em pouco tempo."
-							</p>
-						</article>
-					</div>
-
-					<!--Visão geral-->
-					<p class="text-textLight text-xl font-bold mx-4 mt-2">
-						Visão Geral
-					</p>
-
-					<article class="flex justify-center w-full">
-						<table class="w-[95%] border-separate border-spacing-0.5">
-							<thead class="gap-0.5 text-background font-semibold">
-								<tr>
-									<th class="bg-surface">Sintoma</th>
-									<th class="bg-surface">Duração</th>
-									<th class="bg-surface">Ocorrência</th>
-								</tr>
-							</thead>
-							<tbody class="gap-0.5 text-background">
-								<tr>
-									<th class="bg-textLight">Dor nas Costas</th>
-									<th class="bg-textLight">20 dias</th>
-									<th class="bg-textLight">9 Ocorrências</th>
-								</tr>
-								<tr>
-									<th class="bg-textLight">Estômago Ardendo</th>
-									<th class="bg-textLight">4 dias</th>
-									<th class="bg-textLight">3 Ocorrências</th>
-								</tr>
-							</tbody>
-						</table>
-					</article>
-				</section>
-			</article>
+				<article class="flex justify-center w-full">
+					<table class="w-full border-separate border-spacing-0.5">
+						<thead class="gap-0.5 text-background font-semibold">
+							<tr>
+								<th class="bg-surface">Sintoma</th>
+								<th class="bg-surface">Duração</th>
+								<th class="bg-surface">Ocorrência</th>
+							</tr>
+						</thead>
+						<tbody class="gap-0.5 text-background">
+							<tr>
+								<th class="bg-textLight">Dor nas Costas</th>
+								<th class="bg-textLight">20 dias</th>
+								<th class="bg-textLight">9 Ocorrências</th>
+							</tr>
+							<tr>
+								<th class="bg-textLight">Estômago Ardendo</th>
+								<th class="bg-textLight">4 dias</th>
+								<th class="bg-textLight">3 Ocorrências</th>
+							</tr>
+						</tbody>
+					</table>
+				</article>
+			</section>
 		</form>
 	</BaseDialog>
 
