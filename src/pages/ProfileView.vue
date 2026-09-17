e339f34d61b1a68e06c5685d233e9718f39ffb56
vtkaxx <vtkaxx@proton.me>
2026-09-17 10:01:03 -0300

    finish profile page
    Works on: #10

--- src/pages/ProfileView.vue
+++ src/pages/ProfileView.vue
@@ -21,14 +21,14 @@ authStore.onReady(async (data) => {
 		<NavBar />
 
 		<main
-			class="bg-background size-full flex flex-col gap-10 lg:gap-12 xl:gap-15 p-5 lg:p-7 xl:p-10 overflow-y-auto"
+			class="bg-background size-full flex flex-col gap-10 lg:gap-12 xl:gap-15 p-5 lg:p-7 xl:p-10"
 		>
 			<h1 class="text-4xl text-textLight font-bold text-center lg:text-start">
 				Meu perfil
 			</h1>
 
 			<article class="flex justify-center">
-				<div class="flex flex-col w-200 gap-10">
+				<div class="flex flex-col w-full 2xl:w-300 gap-10">
 					<div class="flex gap-2">
 						<UserPhoto />
 						<div class="flex flex-col justify-center">
@@ -39,62 +39,94 @@ authStore.onReady(async (data) => {
 
 					<hr class="h-1 border-0 bg-textLight w-full m-auto rounded-full opacity-70" />
 
-					<section class="h-full grid grid-cols-2 grid-rows-2 gap-5">
-						<div class="bg-primary rounded-2xl md:col-span-1 p-5 flex flex-col h-full gap-4 text-textLight font-bold">
-							<p class="text-xl">Dados pessoais</p>
+					<section class="max-h-150 flex flex-col overflow-y-auto gap-5 rounded-2xl">
+						<div class="flex flex-col md:flex-row w-full gap-5">
+							<div class="bg-primary w-full rounded-2xl md:col-span-1 p-5 flex flex-col h-full gap-4 text-textLight font-bold">
+								<p class="text-xl">Dados pessoais</p>
+								<div class="grid grid-cols-2 gap-5">
+									<div class="grid col-span-1">
+										<p>{{ userName }} anos</p>
+										<p class="font-normal opacity-70">Idade</p>
+									</div>
+									<div class="grid col-span-1">
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Data de nascimento</p>
+									</div>
+									<div class="grid col-span-1">
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Gênero</p>
+									</div>
+									<div class="grid col-span-1">
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Sexo biológico</p>
+									</div>
+									<div class="grid col-span-1">
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">CPF</p>
+									</div>
+									<div class="grid col-span-1">
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Local</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="bg-primary w-full rounded-2xl md:col-span-1 p-5 flex flex-col h-full gap-4 text-textLight font-bold">
+								<p class="text-xl text-textLight font-bold">Meios de contato</p>
+								<div class="flex flex-col gap-5">
+									<div>
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Telefone</p>
+									</div>
+									<div>
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Email</p>
+									</div>
+
+									<hr class="h-0.5 border-0 bg-textLight w-full m-auto rounded-full opacity-70" />
+
+									<p>Aplicativos conectados</p>
+									<div>
+										<p>{{ userName }}</p>
+										<p class="font-normal opacity-70">Whatsapp</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="bg-primary rounded-2xl md:col-span-2 p-5 flex flex-col justify-between h-full gap-4 text-textLight">
+							<p class="text-xl font-bold">Informações de saúde</p>
+							<div class="grid grid-cols-3 gap-5">
+								<div class="grid col-span-1">
+									<p>{{ userName }}</p>
+									<p class="font-normal opacity-70">Tipo sanguíneo</p>
+								</div>
+								<div class="grid col-span-1">
+									<p>{{ userName }}</p>
+									<p class="font-normal opacity-70">Altura</p>
+								</div>
+								<div class="grid col-span-1">
+									<p>{{ userName }}</p>
+									<p class="font-normal opacity-70">Peso</p>
+								</div>
+							</div>
+
+							<hr class="h-0.5 border-0 bg-textLight w-full m-auto rounded-full opacity-70" />
+
 							<div class="grid grid-cols-2 gap-5">
 								<div class="grid col-span-1">
-									<p>{{ userName }} anos</p>
-									<p class="font-normal opacity-70">Idade</p>
+									<p>{{ userName }}</p>
+									<p>{{ userName }}</p>
+									<p>{{ userName }}</p>
+									<p class="font-normal opacity-70">Alergias</p>
 								</div>
 								<div class="grid col-span-1">
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Data de nascimento</p>
-								</div>
-								<div class="grid col-span-1">
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Gênero</p>
-								</div>
-								<div class="grid col-span-1">
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Sexo biológico</p>
-								</div>
-								<div class="grid col-span-1">
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">CPF</p>
-								</div>
-								<div class="grid col-span-1">
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Local</p>
+									<p>{{ userName }}: {{ userName }}</p>
+									<p>{{ userName }}: {{ userName }}</p>
+									<p>{{ userName }}: {{ userName }}</p>
+									<p class="font-normal opacity-70">Histórico familiar</p>
 								</div>
 							</div>
-						</div>
-
-						<div class="bg-primary rounded-2xl md:col-span-1 p-5 flex flex-col h-full gap-4 text-textLight font-bold">
-							<p class="text-xl text-textLight font-bold">Meios de contato</p>
-							<div class="flex flex-col gap-5">
-								<div>
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Telefone</p>
-								</div>
-								<div>
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Email</p>
-								</div>
-
-								<hr class="h-0.5 border-0 bg-textLight w-full m-auto rounded-full opacity-70" />
-
-								<p>Aplicativos conectados</p>
-								<div>
-									<p>{{ userName }}</p>
-									<p class="font-normal opacity-70">Whatsapp</p>
-								</div>
-							</div>
-						</div>
-
-						<div class="bg-primary rounded-2xl md:col-span-2 p-5 flex flex-col justify-between h-full gap-4">
-							<p class="text-xl text-textLight font-bold">Informações de saúde</p>
-
 						</div>
 					</section>
 				</div>
